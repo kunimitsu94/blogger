29119ada6c841993803135777b11bafb39533eb1
Trevor <tlow94@uw.edu>
2020-07-22 11:53:12 -0700

    added sass

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+  #controller#action
+  get 'pages/contact'
+  # get 'hello', to: 'pages#contact'
+  #/hello leads to our contacts page
+  get 'pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
